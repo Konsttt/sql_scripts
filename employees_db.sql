4f31cd028e11ffa7784e326e988ccc2b56b71f46
Константин Рябов <rkv24@yandex.ru>
2022-09-06 20:29:55 +0300

    Second commit

--- employees_db.sql
+++ employees_db.sql
@@ -1,36 +1,19 @@
-CREATE TABLE IF NOT EXISTS Names (
-	id serial PRIMARY KEY,
-	name_employee varchar(60) NOT NULL,
-	email_emloyee varchar(60) NOT NULL,
-	phone_number varchar(60) NOT NULL
-);
-
 CREATE TABLE IF NOT EXISTS Department (
 	id serial PRIMARY KEY,
-	name_department varchar(60) NOT NULL
+	name_department varchar(60) NOT NULL UNIQUE
 );
 
 CREATE TABLE IF NOT EXISTS Positions (
 	id serial PRIMARY KEY,
-	name_position varchar(60) NOT NULL
+	name_position varchar(60) NOT NULL UNIQUE
 );
 
 CREATE TABLE IF NOT EXISTS Employee (
 	id serial PRIMARY KEY,
-	name_employee integer NOT NULL REFERENCES Names(id),
+	name_employee integer NOT NULL,
 	name_department integer NOT NULL REFERENCES Department(id),
-	name_position integer NOT NULL REFERENCES Positions(id)
-);
-
-CREATE TABLE IF NOT EXISTS Chief (
-	id serial PRIMARY KEY,
-	name_employee integer NOT NULL REFERENCES Names(id),
-	name_department integer NOT NULL REFERENCES Department(id),
-	name_position integer NOT NULL REFERENCES Positions(id)
-);
-
-CREATE TABLE IF NOT EXISTS EmployeeChief (
-	employee_id integer REFERENCES Employee(id),
-	chief_id integer REFERENCES Chief(id),
-	CONSTRAINT emp_ch PRIMARY KEY (employee_id, chief_id)
+	name_position integer NOT NULL REFERENCES Positions(id),
+	email varchar(60) NOT NULL UNIQUE,
+	date_of_employment date CHECK(date_of_employment >= '2000-01-01' AND date_of_employment <= current_date),
+	id_manager integer REFERENCES Employee(id)
 );
--- music_tracks.sql
+++ music_tracks.sql
@@ -1,21 +1,26 @@
 CREATE TABLE IF NOT EXISTS Styles (
 	id serial PRIMARY KEY,
-	style_name varchar(60) NOT NULL
+	style_name varchar(60) UNIQUE NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS Album (
-	id serial PRIMARY KEY,
-	album_name varchar(60) NOT NULL,
-	style_id integer NOT NULL REFERENCES Styles(id),
-	release_year date
-);
-	
 CREATE TABLE IF NOT EXISTS Author (
 	id serial PRIMARY KEY,
 	author_name varchar(60) NOT NULL,
 	nickname varchar(60)
 );
 
+CREATE TABLE IF NOT EXISTS StyleAuthor (
+	style_id integer REFERENCES Styles(id),
+	author_id integer REFERENCES Author(id),
+	CONSTRAINT style_author PRIMARY KEY (style_id, author_id)
+);
+
+CREATE TABLE IF NOT EXISTS Album (
+	id serial PRIMARY KEY,
+	album_name varchar(60) NOT NULL,
+	release_year date CHECK(release_year >= '1900-01-01' AND release_year <= current_date)
+);
+	
 CREATE TABLE IF NOT EXISTS AuthorAlbum (
 	author_id integer REFERENCES Author(id),
 	album_id integer REFERENCES Album(id),
@@ -26,13 +31,14 @@ CREATE TABLE IF NOT EXISTS Track (
 	id serial PRIMARY KEY,
 	track_name varchar(60) NOT NULL,
 	album integer REFERENCES Album(id),
-	release_year date
+	length_track time NOT NULL CHECK(length_track < '01:00:00'),
+	release_year date CHECK(release_year >= '1900-01-01' AND release_year <= current_date)
 );
 
 CREATE TABLE IF NOT EXISTS Collection (
 	id serial PRIMARY KEY,
 	collection_name varchar(60) NOT NULL,
-	release_year date
+	release_year date CHECK(release_year >= '1900-01-01' AND release_year <= current_date)
 );
 
 CREATE TABLE IF NOT EXISTS CollectionTrack(
